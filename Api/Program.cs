42e9f2cbed56475d162673e77d6e8e88fe676cc7
Boles Gamel <boles.gamel123@gmail.com>
2023-07-03 19:00:02 +0300

    End of Section 3

--- Api/Program.cs
+++ Api/Program.cs
@@ -11,12 +11,23 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", Policy =>
+    {
+        Policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .WithOrigins("http://localhost:4200")
+       
+             ;
+});
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+app.UseCors("CorsPolicy");
 app.MapControllers();
 
 app.Run();
